c84bd2539d610b359ededaf6b080ad25ca376a79
hui.wang <hui.wang@funplus.com>
2022-04-08 15:07:52 +0800

    init commit

---
+++ xos/file.go
@@ -0,0 +1,15 @@
+package xos
+
+import "os"
+
+// FileExists 给定的filename是否存在且是一个文件
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
--- xstrings/trim.go
+++ xstrings/trim.go
@@ -15,10 +15,23 @@ var DefaultTrimChars = string([]byte{
 	0xA0, // Non-breaking space.
 })
 
+// Trim 扩展strings.Trim功能，提供默认的mask字符
 func Trim(str string, characterMask ...string) string {
 	trimChars := DefaultTrimChars
 	if len(characterMask) > 0 {
 		trimChars += characterMask[0]
 	}
 	return strings.Trim(str, trimChars)
+}
+
+// SplitAndTrim 将给定字符串分割并trim每一个子元素
+func SplitAndTrim(str, delimiter string, characterMask ...string) []string {
+	array := make([]string, 0)
+	for _, v := range strings.Split(str, delimiter) {
+		v = Trim(v, characterMask...)
+		if v != "" {
+			array = append(array, v)
+		}
+	}
+	return array
 }
