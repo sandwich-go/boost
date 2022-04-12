b7cb53a69e68fde0b2299a19d800f903d8e91ffc
hui.wang <hui.wang@funplus.com>
2022-04-12 12:42:40 +0800

    refactor: add StringsRemoveRepeated

--- xslice/string.go
+++ xslice/string.go
@@ -61,3 +61,51 @@ func StringsAddSuffix(s []string, suffix string) []string {
 	}
 	return out
 }
+
+// StringsRemoveRepeated 移除重复元素
+func StringsRemoveRepeated(slc []string) []string {
+	if len(slc) == 0 {
+		return slc
+	}
+	if len(slc) < 1024 {
+		// 切片长度小于1024的时候，循环来过滤
+		return removeRepeatByLoop(slc)
+	} else {
+		// 大于的时候，通过map来过滤
+		return removeRepeatByMap(slc)
+	}
+}
+func removeRepeatByMap(slc []string) []string {
+	if len(slc) == 0 {
+		return slc
+	}
+	result := []string{}
+	tempMap := map[string]byte{} // 存放不重复主键
+	for _, e := range slc {
+		l := len(tempMap)
+		tempMap[e] = 0
+		if len(tempMap) != l { // 加入map后，map长度变化，则元素不重复
+			result = append(result, e)
+		}
+	}
+	return result
+}
+func removeRepeatByLoop(slc []string) []string {
+	if len(slc) == 0 {
+		return slc
+	}
+	result := []string{} // 存放结果
+	for i := range slc {
+		flag := true
+		for j := range result {
+			if slc[i] == result[j] {
+				flag = false // 存在重复元素，标识为false
+				break
+			}
+		}
+		if flag { // 标识为false，不添加进结果
+			result = append(result, slc[i])
+		}
+	}
+	return result
+}
