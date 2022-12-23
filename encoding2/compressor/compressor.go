d96822241d12923898a1b592312b9b987037c411
祝黄清 <huangqing.zhu@centurygame.com>
2022-12-23 15:36:50 +0800

    feat: 增加 compressor和encrypt codec

--- encoding2/compressor/compressor.go
+++ encoding2/compressor/compressor.go
@@ -1,23 +1,50 @@
 package compressor
 
-import "io"
+import (
+	"errors"
+	"github.com/sandwich-go/boost/encoding2"
+)
 
-type WriteFlushCloser interface {
-	io.WriteCloser
-	Flush() error
+var (
+	errCompressorCodecMarshalParam   = errors.New("compressor codec marshal must be []byte parameter")
+	errCompressorCodecUnmarshalParam = errors.New("compressor codec unmarshal must be *[]byte parameter")
+	errCompressorCodecNoFound        = errors.New("compressor codec not found")
+)
+
+type CompressType byte
+
+const (
+	CompressNone CompressType = iota
+	CompressGzip
+	CompressSnappy
+)
+
+var compressorCodecs = map[CompressType]encoding2.Codec{
+	CompressNone:   DummyCodec,
+	CompressGzip:   GzipCodec,
+	CompressSnappy: SnappyCodec,
 }
 
-type Compressor interface {
-	Compress(w io.Writer) (WriteFlushCloser, error)
-	Decompress(r io.Reader) (io.ReadCloser, error)
-	Name() string
+type Codec struct {
+	compressType CompressType
 }
 
-var registeredCompressor = make(map[string]Compressor)
-
-func RegisterCompressor(c Compressor) {
-	registeredCompressor[c.Name()] = c
+func NewCodec(compressType CompressType) encoding2.Codec {
+	return Codec{compressType: compressType}
 }
-func GetCompressor(name string) Compressor {
-	return registeredCompressor[name]
+
+func (c Codec) Name() string { return "compressor" }
+func (c Codec) Marshal(v interface{}) ([]byte, error) {
+	cc, ok1 := compressorCodecs[c.compressType]
+	if !ok1 {
+		return nil, errCompressorCodecNoFound
+	}
+	return cc.Marshal(v)
+}
+func (c Codec) Unmarshal(bytes []byte, v interface{}) error {
+	cc, ok1 := compressorCodecs[c.compressType]
+	if !ok1 {
+		return errCompressorCodecNoFound
+	}
+	return cc.Unmarshal(bytes, v)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/buger/jsonparser v1.1.1
 	github.com/golang/protobuf v1.5.2
+	github.com/golang/snappy v0.0.4
 	github.com/smartystreets/goconvey v1.7.2
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
