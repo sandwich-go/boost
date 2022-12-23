ad108ebc249b511f94e55c8ad8015f3808f57537
祝黄清 <huangqing.zhu@centurygame.com>
2022-12-23 15:44:04 +0800

    refactor: 移除encoding2.init

--- encoding2/init.go
+++
@@ -1,9 +0,0 @@
-package encoding2
-
-import (
-	"github.com/sandwich-go/boost/encoding2/compressor"
-)
-
-func GetCompressor(name string) compressor.Compressor {
-	return compressor.GetCompressor(name)
-}
