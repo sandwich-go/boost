046e4e71d3c93d5e9e72799a99bc12d9da3070e5
祝黄清 <huangqing.zhu@centurygame.com>
2023-01-05 17:02:57 +0800

    fix: aes

--- encryption/aes/aes.go
+++ encryption/aes/aes.go
@@ -1,35 +1,13 @@
 package aes
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"errors"
+	"github.com/coreos/pkg/cryptoutil"
 )
 
 func CBCEncrypt(src []byte, key []byte) ([]byte, error) {
-	if len(src) == 0 {
-		return nil, errors.New("CBCEncrypt empty src")
-	}
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	src = Padding(src, block.BlockSize())
-	iv := key[:block.BlockSize()]
-	cipher.NewCBCEncrypter(block, iv).CryptBlocks(src, src)
-	return src, nil
+	return cryptoutil.AESEncrypt(src, key)
 }
 
 func CBCDecrypt(src []byte, key []byte) ([]byte, error) {
-	if len(src) == 0 {
-		return nil, nil
-	}
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	iv := key[:block.BlockSize()]
-	cipher.NewCBCDecrypter(block, iv).CryptBlocks(src, src)
-	src = UnPadding(src)
-	return src, nil
+	return cryptoutil.AESDecrypt(src, key)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/buger/jsonparser v1.1.1
+	github.com/coreos/pkg v0.0.0-20220810130054-c7d1c02cb6cf
 	github.com/golang/protobuf v1.5.2
 	github.com/golang/snappy v0.0.4
 	github.com/smartystreets/goconvey v1.7.2
