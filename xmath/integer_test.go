c8a6e095aa0a4352a4de16fd07f248000bf64a8d
祝黄清 <huangqing.zhu@centurygame.com>
2023-02-06 16:07:27 +0800

    chore: xmath readme

--- xmath/integer_test.go
+++ xmath/integer_test.go
@@ -6,6 +6,9 @@ func TestMustParseUint64(t *testing.T) {
 	if v := MustParseUint64("12345"); v != 12345 {
 		t.Errorf(`MustParseUint64("12345") = %d, want 12345`, v)
 	}
+	if v := MustParseUint64("0x16"); v != 22 {
+		t.Errorf(`MustParseUint64("0x16") = %d, want 22`, v)
+	}
 }
 
 func TestMustParseUint64Panic(t *testing.T) {
