beb36fe2f9033860cb9de4ca183c71914b2a195a
祝黄清 <huangqing.zhu@centurygame.com>
2023-02-23 12:14:37 +0800

    fix: 修改 snappy 压缩错误

--- xcompress/snappy.go
+++ xcompress/snappy.go
@@ -1,9 +1,9 @@
 package xcompress
 
 import (
-	"bytes"
+	"fmt"
 	"github.com/golang/snappy"
-	"io/ioutil"
+	"github.com/sandwich-go/boost/xpanic"
 )
 
 type snappyCompressor struct{}
@@ -16,23 +16,19 @@ func (c *snappyCompressor) Flat(data []byte) ([]byte, error) {
 	if len(data) == 0 {
 		return data, nil
 	}
-	var buffer bytes.Buffer
-	writer := snappy.NewBufferedWriter(&buffer)
-	_, err := writer.Write(data)
-	if err != nil {
-		_ = writer.Close()
-		return nil, err
-	}
-	err = writer.Close()
-	if err != nil {
-		return nil, err
-	}
-	return buffer.Bytes(), nil
+	var err error
+	var out []byte
+	xpanic.Try(func() {
+		out = snappy.Encode(nil, data)
+	}).Catch(func(e xpanic.E) {
+		err = fmt.Errorf("snappy flat error, %v", e)
+	})
+	return out, err
 }
 
 func (c *snappyCompressor) Inflate(data []byte) ([]byte, error) {
 	if len(data) == 0 {
 		return data, nil
 	}
-	return ioutil.ReadAll(snappy.NewReader(bytes.NewReader(data)))
+	return snappy.Decode(nil, data)
 }
