92cf01c40fc2b0cf15b817fda3c3fcdb87770775
祝黄清 <huangqing.zhu@centurygame.com>
2023-02-24 17:57:30 +0800

    feat(xcompress): 增加默认的Flat/Inflate方法

--- xcompress/compressor.go
+++ xcompress/compressor.go
@@ -20,3 +20,9 @@ type Compressor interface {
 
 // Default 默认的压缩器，使用 GZIP 方式，进行 DefaultCompression 等级的压缩与解压缩
 var Default = MustNew(WithType(GZIP), WithLevel(DefaultCompression))
+
+// Flat 默认的压缩器压缩二进制数据
+func Flat(data []byte) ([]byte, error) { return Default.Flat(data) }
+
+// Inflate 默认的解压二进制数据
+func Inflate(data []byte) ([]byte, error) { return Default.Inflate(data) }
