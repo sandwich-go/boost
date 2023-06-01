75494bda017ff07369e8f79b48255fe7f3c3ac93
Daming Yang <daming.yang@centurygame.com>
2023-06-01 16:08:17 +0800

    refactor: xerror 支持os.IsTimeout

--- xerror/xerror.go
+++ xerror/xerror.go
@@ -1,19 +1,45 @@
 package xerror
 
+import "os"
+
 var IsErrorWithStack = false
 
 // Error struct
 type Error struct {
-	err       error  // 底层错误
-	text      string // 错误信息
-	code      int32  // 错误码
-	logic     bool   // 是否为逻辑层异常，主要为sandwich 的queue模式服务
-	callStack stack  // 堆栈信息，内部数据自动生成
-	skip      int    // error skip
+	err        error  // 底层错误
+	text       string // 错误信息
+	code       int32  // 错误码
+	logic      bool   // 是否为逻辑层异常，主要为sandwich 的queue模式服务
+	callStack  stack  // 堆栈信息，内部数据自动生成
+	skip       int    // error skip
+	setTimeout bool   // 是否设置超时, 如果明确设置超时状态，则不再根据底层错误判断
+	timeout    bool   // 是否为超时错误
 }
 
 // Logic 是否为逻辑层错误
 func (cc *Error) Logic() bool { return cc.logic }
+
+// Timeout 是否为超时错误 os.IsTimeout
+func (cc *Error) Timeout() bool {
+	if !cc.setTimeout && cc.err != nil {
+		return cc.timeout || os.IsTimeout(cc.err)
+	}
+	return cc.timeout
+}
+
+// SetTimeout 设定为超时
+func (cc *Error) SetTimeout() *Error {
+	cc.setTimeout = true
+	cc.timeout = true
+	return cc
+}
+
+// UnsetTimeout 设定为非超时
+func (cc *Error) UnsetTimeout() *Error {
+	cc.setTimeout = true
+	cc.timeout = false
+	return cc
+}
 
 // WithStack 设置堆栈
 func (cc *Error) WithStack() *Error {
@@ -55,6 +81,12 @@ func WithText(v string) ErrorOption { return func(cc *Error) { cc.text = v } }
 func WithCode(v int32) ErrorOption  { return func(cc *Error) { cc.code = v } }
 func WithLogic(v bool) ErrorOption  { return func(cc *Error) { cc.logic = v } }
 func WithSkip(v int) ErrorOption    { return func(cc *Error) { cc.skip = v } }
+func WithTimeout(v bool) ErrorOption {
+	return func(cc *Error) {
+		cc.setTimeout = true
+		cc.timeout = v
+	}
+}
 
 // WithStack option func for stack
 func WithStack() ErrorOption {
