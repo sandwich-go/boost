ecda819f255fbbc45268a7391f7563ffc913c984
hui.wang <hui.wang@centurygame.com>
2024-04-17 16:22:23 +0800

    feat: smap any support

--- xcontainer/smap/any.go
+++ xcontainer/smap/any.go
@@ -14,7 +14,7 @@ import (
 var DefaultShardCount = uint64(32)
 
 type mapKey interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | string
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | complex64 | complex128 | string
 }
 
 type Concurrent[K mapKey, V any] struct {
