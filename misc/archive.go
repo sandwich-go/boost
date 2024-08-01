6d42bc5102bca24778536b6cb3f582c57ac2aa66
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-08-01 11:47:38 +0800

    feat: 增加 archive

---
+++ misc/archive.go
@@ -0,0 +1,38 @@
+package misc
+
+import (
+	"github.com/sandwich-go/boost/xpool"
+)
+
+// Archive orm record 回滚使用的协助类
+type Archive[T any] struct {
+	Data T
+}
+
+type ArchivePool[T any] struct {
+	pool *xpool.Pool[*Archive[T]]
+}
+
+// NewArchivePool 创建 Archive 池
+func NewArchivePool[T any]() *ArchivePool[T] {
+	return &ArchivePool[T]{
+		pool: xpool.NewPool(func() *Archive[T] {
+			return &Archive[T]{}
+		}),
+	}
+}
+
+func (p *ArchivePool[T]) Get(data *T) *Archive[T] {
+	a := p.pool.Get()
+	a.Data = *data
+	return a
+}
+
+func (p *ArchivePool[T]) Put(a *Archive[T]) {
+	if a == nil {
+		return
+	}
+	var zero T
+	a.Data = zero
+	p.pool.Put(a)
+}
