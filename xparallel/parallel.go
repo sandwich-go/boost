f22ef259daaab9643bd9e48d846c1b4afa936862
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-08-14 14:05:39 +0800

    feat: 增加并发处理帮助类

---
+++ xparallel/parallel.go
@@ -0,0 +1,41 @@
+package xparallel
+
+import "sync"
+
+func worker[V any](wg *sync.WaitGroup, ch chan V, fn func(k V)) {
+	for v := range ch {
+		fn(v)
+	}
+	wg.Done()
+}
+
+func closeThenParallel[V any](maxp int, ch chan V, fn func(k V)) {
+	close(ch)
+	concurrency := len(ch)
+	if concurrency > maxp {
+		concurrency = maxp
+	}
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+	for i := 1; i < concurrency; i++ {
+		go worker(&wg, ch, fn)
+	}
+	worker(&wg, ch, fn)
+	wg.Wait()
+}
+
+func K[K comparable, V any](maxp int, p map[K]V, fn func(k K)) {
+	ch := make(chan K, len(p))
+	for k := range p {
+		ch <- k
+	}
+	closeThenParallel(maxp, ch, fn)
+}
+
+func V[K comparable, V any](maxp int, p map[K]V, fn func(v V)) {
+	ch := make(chan V, len(p))
+	for _, v := range p {
+		ch <- v
+	}
+	closeThenParallel(maxp, ch, fn)
+}
