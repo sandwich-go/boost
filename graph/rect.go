a23fddcc7870a674336278e3a59a59a9ab283502
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-08-26 11:55:23 +0800

    test: 增加 test

--- graph/rect.go
+++ graph/rect.go
@@ -264,10 +264,10 @@ func RectFromCenterSize[T Number](center, size Point[T]) Rectangle[T] {
 	return Rect(center.x-size.x, center.y-center.y, center.x+size.x, center.y+size.y)
 }
 
-// RectFromBottomLeftSize constructs a rectangle with the given BottomLeft and size.
+// RectFromMinSize constructs a rectangle with the given BottomLeft and size.
 // Both dimensions of size must be non-negative.
-func RectFromBottomLeftSize[T Number](bottomLeft, size Point[T]) Rectangle[T] {
-	return Rect(bottomLeft.x, bottomLeft.y, bottomLeft.x+size.x, bottomLeft.y+size.y)
+func RectFromMinSize[T Number](min, size Point[T]) Rectangle[T] {
+	return Rect(min.x, min.y, min.x+size.x, min.y+size.y)
 }
 
 // HelixRectRangeFromCenterAndMargin 由center节点逆时针螺旋由内向外访问margin区域内的所有节点
