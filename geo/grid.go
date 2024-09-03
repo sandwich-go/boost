2695b2a58f6b21bee4b37d05aaaf956ce129e622
biyongze <yongze.bi@centurygame.com>
2024-09-03 19:05:50 +0800

    rename(geo): 对齐概念

--- geo/grid.go
+++ geo/grid.go
@@ -10,23 +10,23 @@ import (
 )
 
 var (
-	ErrInvalidCellIndex = errors.New("invalid cell index")
+	ErrInvalidGridIndex = errors.New("invalid grid index")
 )
 
 //go:generate mockgen -source grid.go -package geo -destination grid_mock_test.go
 
-// Cell 网格中的基础单元格
-type Cell interface{}
+// Grid 网格中的基础单元格 地格
+type Grid interface{}
 
-// CellBuilder 基础单元格工厂
-type CellBuilder interface {
-	Build(x, y int) Cell
+// GridBuilder 基础单元格工厂
+type GridBuilder interface {
+	Build(x, y int) Grid
 }
 
-// Grid 网格，包含二维的 cell
-type Grid[T graph.Number] struct {
-	cells    [][]Cell
-	cellSize T
+// Block 网格，包含二维的 Grid
+type Block[T graph.Number] struct {
+	grids    [][]Grid
+	gridSize T
 	width    T
 	height   T
 }
@@ -34,72 +34,72 @@ type Grid[T graph.Number] struct {
 // G 新建一个网格
 // width 地图的宽
 // height 地图的高
-// cellSize 基础单元格大小
-// cellBuilder 基础单元格工厂
-func G[T graph.Number](width, height, cellSize T, cellBuilder CellBuilder) *Grid[T] {
-	g := &Grid[T]{width: width, height: height, cellSize: cellSize}
-	g.mustInitialize(cellBuilder)
+// gridSize 基础单元格大小
+// gridBuilder 基础单元格工厂
+func G[T graph.Number](width, height, gridSize T, gridBuilder GridBuilder) *Block[T] {
+	g := &Block[T]{width: width, height: height, gridSize: gridSize}
+	g.mustInitialize(gridBuilder)
 	return g
 }
 
-func (g *Grid[T]) toSum(t T) int   { return int(math.Ceil(float64(t) / float64(g.cellSize))) }
-func (g *Grid[T]) toIndex(t T) int { return int(math.Floor(float64(t) / float64(g.cellSize))) }
-func (g *Grid[T]) toPoint(x int) T { return T(x) * g.cellSize }
+func (g *Block[T]) toSum(t T) int   { return int(math.Ceil(float64(t) / float64(g.gridSize))) }
+func (g *Block[T]) toIndex(t T) int { return int(math.Floor(float64(t) / float64(g.gridSize))) }
+func (g *Block[T]) toPoint(x int) T { return T(x) * g.gridSize }
 
-func (g *Grid[T]) mustInitialize(cellBuilder CellBuilder) {
+func (g *Block[T]) mustInitialize(gridBuilder GridBuilder) {
 	xNum := g.toSum(g.width)
 	yNum := g.toSum(g.height)
-	g.cells = make([][]Cell, xNum)
+	g.grids = make([][]Grid, xNum)
 	for i := 0; i < xNum; i++ {
-		g.cells[i] = make([]Cell, yNum)
+		g.grids[i] = make([]Grid, yNum)
 		for j := 0; j < yNum; j++ {
-			g.cells[i][j] = cellBuilder.Build(i, j)
+			g.grids[i][j] = gridBuilder.Build(i, j)
 		}
 	}
 }
 
-func (g *Grid[T]) checkCellIndex(x, y int) bool {
-	return x >= 0 && x < len(g.cells) && y >= 0 && y < len(g.cells[0])
+func (g *Block[T]) checkGridIndex(x, y int) bool {
+	return x >= 0 && x < len(g.grids) && y >= 0 && y < len(g.grids[0])
 }
 
-func (g *Grid[T]) checkPoint(point graph.Point[T]) bool {
+func (g *Block[T]) checkPoint(point graph.Point[T]) bool {
 	return cmp.Compare(point.X(), 0) >= 0 && cmp.Compare(point.X(), g.width) < 0 && cmp.Compare(point.Y(), 0) >= 0 && cmp.Compare(point.Y(), g.height) < 0
 }
 
-// GetCellByIndex 通过基础单元格坐标索引 x,y 获取基础单元格
-func (g *Grid[T]) GetCellByIndex(x, y int) (Cell, error) {
-	if !g.checkCellIndex(x, y) {
-		return nil, xerror.Wrap(ErrInvalidCellIndex, "index: (%d,%d)", x, y)
+// GetGridByIndex 通过基础单元格坐标索引 x,y 获取基础单元格
+func (g *Block[T]) GetGridByIndex(x, y int) (Grid, error) {
+	if !g.checkGridIndex(x, y) {
+		return nil, xerror.Wrap(ErrInvalidGridIndex, "index: (%d,%d)", x, y)
 	}
-	return g.cells[x][y], nil
+	return g.grids[x][y], nil
 }
 
-// MustGetCellByIndex 通过基础单元格坐标索引 x,y 获取基础单元格
+// MustGetGridByIndex 通过基础单元格坐标索引 x,y 获取基础单元格
 // 找不到则 panic
-func (g *Grid[T]) MustGetCellByIndex(x, y int) Cell {
-	c, err := g.GetCellByIndex(x, y)
+func (g *Block[T]) MustGetGridByIndex(x, y int) Grid {
+	c, err := g.GetGridByIndex(x, y)
 	xpanic.WhenError(err)
 	return c
 }
 
-// GetCellByPoint 通过坐标， 获取基础单元格
-func (g *Grid[T]) GetCellByPoint(point graph.Point[T]) (Cell, error) {
+// GetGridByPoint 通过坐标， 获取基础单元格
+func (g *Block[T]) GetGridByPoint(point graph.Point[T]) (Grid, error) {
 	if !g.checkPoint(point) {
-		return nil, xerror.Wrap(ErrInvalidCellIndex, "point: %s", point)
+		return nil, xerror.Wrap(ErrInvalidGridIndex, "point: %s", point)
 	}
-	return g.GetCellByIndex(g.toIndex(point.X()), g.toIndex(point.Y()))
+	return g.GetGridByIndex(g.toIndex(point.X()), g.toIndex(point.Y()))
 }
 
-// MustGetCellByPoint 通过坐标， 获取基础单元格
+// MustGetGridByPoint 通过坐标， 获取基础单元格
 // 找不到则 panic
-func (g *Grid[T]) MustGetCellByPoint(point graph.Point[T]) Cell {
-	c, err := g.GetCellByPoint(point)
+func (g *Block[T]) MustGetGridByPoint(point graph.Point[T]) Grid {
+	c, err := g.GetGridByPoint(point)
 	xpanic.WhenError(err)
 	return c
 }
 
-// RangeCellInRectangle 在一个矩形中遍历基础单元格
-func (g *Grid[T]) RangeCellInRectangle(rect graph.Rectangle[T], with func(p Cell) bool) {
+// RangeGridInRectangle 在一个矩形中遍历基础单元格
+func (g *Block[T]) RangeGridInRectangle(rect graph.Rectangle[T], with func(p Grid) bool) {
 	var minP, maxP = rect.Min(), rect.Max()
 	var minX, minY, maxX, maxY T = 0, 0, g.width, g.height
 	if x := minP.X(); cmp.Compare(x, minX) > 0 {
@@ -108,12 +108,12 @@ func (g *Grid[T]) RangeCellInRectangle(rect graph.Rectangle[T], with func(p Cell
 	if y := minP.Y(); cmp.Compare(y, minY) > 0 {
 		minY = y
 	}
-	// x + g.cellSize - 1 保证右下角的单元格也会被遍历到
-	if x := maxP.X() + g.cellSize - 1; cmp.Compare(x, maxX) < 0 {
+	// x + g.gridSize - 1 保证右下角的单元格也会被遍历到
+	if x := maxP.X() + g.gridSize - 1; cmp.Compare(x, maxX) < 0 {
 		maxX = x
 	}
-	// y + g.cellSize - 1 保证右下角的单元格也会被遍历到
-	if y := maxP.Y() + g.cellSize - 1; cmp.Compare(y, maxY) < 0 {
+	// y + g.gridSize - 1 保证右下角的单元格也会被遍历到
+	if y := maxP.Y() + g.gridSize - 1; cmp.Compare(y, maxY) < 0 {
 		maxY = y
 	}
 	for x := g.toIndex(minX); x < g.toSum(maxX); x++ {
@@ -122,7 +122,7 @@ func (g *Grid[T]) RangeCellInRectangle(rect graph.Rectangle[T], with func(p Cell
 			if !ok {
 				continue
 			}
-			c, err := g.GetCellByIndex(x, y)
+			c, err := g.GetGridByIndex(x, y)
 			if err != nil {
 				// 不合法的，丢弃
 				continue
@@ -134,20 +134,20 @@ func (g *Grid[T]) RangeCellInRectangle(rect graph.Rectangle[T], with func(p Cell
 	}
 }
 
-// RangeCellInLine 在一个线中遍历基础单元格
-func (g *Grid[T]) RangeCellInLine(l graph.Line[T], with func(p Cell) bool) {
-	var gotCells = make(map[int]struct{}, 0)
+// RangeGridInLine 在一个线中遍历基础单元格
+func (g *Block[T]) RangeGridInLine(l graph.Line[T], with func(p Grid) bool) {
+	var gotGrids = make(map[int]struct{}, 0)
 	l.RangePoints(func(point graph.Point[T]) bool {
 		x := g.toIndex(point.X())
 		y := g.toIndex(point.Y())
-		n := x*len(g.cells) + y
-		_, ok := gotCells[n]
+		n := x*len(g.grids) + y
+		_, ok := gotGrids[n]
 		if ok {
 			// 已经遍历过，丢弃
 			return true
 		}
-		gotCells[n] = struct{}{}
-		c, err := g.GetCellByIndex(x, y)
+		gotGrids[n] = struct{}{}
+		c, err := g.GetGridByIndex(x, y)
 		if err != nil {
 			// 不合法的，丢弃
 			return true
--- geo/grid_test.go
+++ geo/grid_test.go
@@ -11,76 +11,76 @@ func TestGrid(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	Convey("test grid", t, func() {
-		b := NewMockCellBuilder(ctrl)
-		b.EXPECT().Build(gomock.Any(), gomock.Any()).DoAndReturn(func(x, y int) Cell {
-			c := NewMockCell(ctrl)
+		b := NewMockGridBuilder(ctrl)
+		b.EXPECT().Build(gomock.Any(), gomock.Any()).DoAndReturn(func(x, y int) Grid {
+			c := NewMockGrid(ctrl)
 			return c
 		}).AnyTimes()
 
-		var width, height, cellSize int64 = 1200, 1200, 3
-		g := G[int64](width, height, cellSize, b)
+		var width, height, GridSize int64 = 1200, 1200, 3
+		g := G[int64](width, height, GridSize, b)
 
-		c, err := g.GetCellByIndex(int(width/cellSize+1), int(height/cellSize))
+		c, err := g.GetGridByIndex(int(width/GridSize+1), int(height/GridSize))
 		So(err, ShouldNotBeNil)
-		c, err = g.GetCellByPoint(graph.P[int64](width+1, height+1))
+		c, err = g.GetGridByPoint(graph.P[int64](width+1, height+1))
 		So(err, ShouldNotBeNil)
 
 		So(func() {
-			_ = g.MustGetCellByIndex(int(width/cellSize+1), int(height/cellSize))
+			_ = g.MustGetGridByIndex(int(width/GridSize+1), int(height/GridSize))
 		}, ShouldPanic)
 
 		So(func() {
-			_ = g.MustGetCellByPoint(graph.P[int64](width+1, height+1))
+			_ = g.MustGetGridByPoint(graph.P[int64](width+1, height+1))
 		}, ShouldPanic)
 
-		c, err = g.GetCellByIndex(0, 1)
+		c, err = g.GetGridByIndex(0, 1)
 		So(err, ShouldBeNil)
 		So(c, ShouldNotBeNil)
 
 		var count int
-		g.RangeCellInRectangle(graph.Rect[int64](0, 0, 2, 2), func(p Cell) bool {
+		g.RangeGridInRectangle(graph.Rect[int64](0, 0, 2, 2), func(p Grid) bool {
 			count++
 			return true
 		})
 		So(count, ShouldEqual, 1)
 
 		count = 0
-		g.RangeCellInRectangle(graph.Rect[int64](0, 0, 3, 3), func(p Cell) bool {
+		g.RangeGridInRectangle(graph.Rect[int64](0, 0, 3, 3), func(p Grid) bool {
 			count++
 			return true
 		})
 		So(count, ShouldEqual, 4)
 
 		count = 0
-		g.RangeCellInRectangle(graph.Rect[int64](0, 0, 4, 4), func(p Cell) bool {
+		g.RangeGridInRectangle(graph.Rect[int64](0, 0, 4, 4), func(p Grid) bool {
 			count++
 			return true
 		})
 		So(count, ShouldEqual, 4)
 
 		count = 0
-		g.RangeCellInRectangle(graph.Rect[int64](0, 0, 6, 6), func(p Cell) bool {
+		g.RangeGridInRectangle(graph.Rect[int64](0, 0, 6, 6), func(p Grid) bool {
 			count++
 			return true
 		})
 		So(count, ShouldEqual, 9)
 
 		count = 0
-		g.RangeCellInRectangle(graph.Rect[int64](0, 0, 7, 7), func(p Cell) bool {
+		g.RangeGridInRectangle(graph.Rect[int64](0, 0, 7, 7), func(p Grid) bool {
 			count++
 			return true
 		})
 		So(count, ShouldEqual, 9)
 
 		count = 0
-		g.RangeCellInLine(graph.L[int64](graph.P[int64](0, 0), graph.P[int64](2, 2)), func(p Cell) bool {
+		g.RangeGridInLine(graph.L[int64](graph.P[int64](0, 0), graph.P[int64](2, 2)), func(p Grid) bool {
 			count++
 			return true
 		})
 		So(count, ShouldEqual, 1)
 
 		count = 0
-		g.RangeCellInLine(graph.L[int64](graph.P[int64](0, 0), graph.P[int64](4, 4)), func(p Cell) bool {
+		g.RangeGridInLine(graph.L[int64](graph.P[int64](0, 0), graph.P[int64](4, 4)), func(p Grid) bool {
 			count++
 			return true
 		})
