ca2ac85d2fce3bdb9531269cb37cb398304dbc49
hui.wang <hui.wang@centurygame.com>
2025-03-16 18:59:18 +0800

    feat: ip prefer vpn

--- xos/env.go
+++ xos/env.go
@@ -1,6 +1,9 @@
 package xos
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 // EnvGet 获取环境变量值，如果未找到，则返回 def 值
 func EnvGet(key string, def ...string) string {
@@ -9,4 +12,20 @@ func EnvGet(key string, def ...string) string {
 		return def[0]
 	}
 	return v
+}
+
+// EnvGetCaseInsensitive 查找环境变量，大小写不敏感
+// 如果未找到，则返回 def 值，如未提供def值则返回空
+func EnvGetCaseInsensitive(key string, def ...string) string {
+	upperKey := strings.ToUpper(key)
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		if len(pair) == 2 && strings.ToUpper(strings.TrimSpace(pair[0])) == upperKey {
+			return pair[1]
+		}
+	}
+	if len(def) > 0 {
+		return def[0]
+	}
+	return ""
 }
