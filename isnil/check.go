fd10e7c053f0d12b7cf2ed604b5496e9c4828be4
hui.wang <hui.wang@centurygame.com>
2025-04-02 16:22:01 +0800

    fix: nil check using refect2

--- isnil/check.go
+++ isnil/check.go
@@ -1,21 +1,10 @@
 package isnil
 
-import "reflect"
-
-type isNiler interface {
-	IsNil() bool
-}
+import (
+	"github.com/modern-go/reflect2"
+)
 
 // Check checks if any is nil.
 func Check(any interface{}) bool {
-	if any == nil {
-		return true
-	}
-
-	if checker, ok := any.(isNiler); ok {
-		return checker.IsNil()
-	}
-
-	v := reflect.ValueOf(any)
-	return v.Kind() == reflect.Ptr && v.IsNil()
+	return reflect2.IsNil(any)
 }
